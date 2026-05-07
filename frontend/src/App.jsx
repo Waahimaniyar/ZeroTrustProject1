b520cc2842c558dee3c0760e6f309e053b7836ea
WAHEED J MANIYAR <waheedjmaniyar@WAHEEDs-MacBook-Air.local>
2026-05-07 13:56:22 +0530

    Day 1 completed

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>Zero Trust Dashboard</h1>
+      <h2>Frontend Connected Successfully!</h2>
+    </div>
+  );
+}
+
+export default App;
